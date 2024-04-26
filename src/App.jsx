fd10c428ae52970131d95fdf7a7ed84c41302a56
sanda94 <lsandaruwan388@gmail.com>
2024-04-26 10:29:34 +0800

    inite

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import About from './components/About'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Technologies from './components/Technologies'
-import Experience from './components/Experience'
-import Project from './components/Project'
-import Contact from './components/Contact'
+// App.jsx
+import Navbar from './components/Navbar.jsx';
+import About from './components/About.jsx';
+import Hero from './components/Hero.jsx';
+import Technologies from './components/Technologies.jsx';
+import Experience from './components/Experience.jsx';
+import Project from './components/Project.jsx';
+import Contact from './components/Contact.jsx';
 
 const App = () => {
   return (
     <div>
       <div className="flixed top-0 -z-10 h h-full w w-full"></div>
-    <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
-    <div className="conainer mx-auto px-8">
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Technologies/>
-      <Experience/>
-      <Project/>
-      <Contact/>
+      <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+      <div className="conainer mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <About />
+        <Technologies />
+        <Experience />
+        <Project />
+        <Contact />
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,8 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of Sri Lanka And 6 Mothes Of Singapore hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
@@ -11,28 +12,28 @@ export const EXPERIENCES = [
   {
     year: "2023 - Present",
     role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    company: "XPAC TECHNOLOGY.",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB",   "React Native"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
+    year: "2017 - 2019",
+    role: "Full Stack Developer",
+    company: "EXTREME IT SOLUTIONS PTE LTD, SRI LANKA",
     description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
+    year: "2015 - 2017",
+    role: "Frontend Developer",
+    company: "EXTREME IT SOLUTIONS PTE LTD, SRI LANKA",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
   },
   {
-    year: "2020 - 2021",
+    year: "2013 - 2015",
     role: "Software Engineer",
-    company: "Paypal",
+    company: "EXTREME IT SOLUTIONS PTE LTD, SRI LANKA",
     description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
     technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
@@ -40,28 +41,28 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Xpac Inventry Management system",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "React Native"],
   },
   {
-    title: "Task Management App",
+    title: "Shelter Me Mobile App & Web App ",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+      "Investigate the Animal Tracking system requirements and associated support reference papers,Analyze the system requirements and create a functional specification paper,An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking Animals.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
-    title: "Portfolio Website",
+    title: "NOSQL DATABASE BASED ON PSB ACADEMY'S OPERATION THE FINAL PROJECT OF THE BIG DATA MODULE",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "Cloud database and the step-by-step to create a database 5 collections, 5 Constraints Cloud storage and the step-by-step to create storage for Validation Rule",
+    technologies: ["Mongo DB", "React JS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "E-COMMERCE WEBSITE.",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
