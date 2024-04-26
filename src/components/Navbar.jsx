2a6dc15aa82d840b954a88913485da8d8f9c092c
sanda94 <lsandaruwan388@gmail.com>
2024-04-26 13:31:44 +0800

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
@@ -9,28 +9,22 @@ const Navbar = () => {
   return (
     <nav className="bg-red-300 mb-5 flex items-start justify-between py-6">
       <motion.div 
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 1.1 }}
-      drag="x"
-      dragConstraints={{ left: -100, right: 100 }}
-        initial="hidden"
-        animate="visible"
-       className="flex flex-shrink-0 items-start">
-        <img className="mx-6 w-40 rounded-2xl" src={logo} alt="logo"/>
-      </motion.div>
-      <motion.div 
-      
-      whileHover={{ scale: 1.2 }}
+        whileHover={{ scale: 1.2 }}
         whileTap={{ scale: 1.1 }}
         drag="x"
         dragConstraints={{ left: -100, right: 100 }}
-          initial="hidden"
-          animate="visible"
-          className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaTwitter />
-        <FaInstagram />
+        initial="hidden"
+        animate="visible"
+        className="flex flex-shrink-0 items-start">
+        <img className="mx-6 w-40 rounded-2xl" src={logo} alt="logo"/>
+      </motion.div>
+      <motion.div 
+        animate="visible"
+        className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/"><FaLinkedin /></a>
+        <a href="https://github.com/"><FaGithub /></a>
+        <a href="https://facebook.com/"><FaFacebook /></a>
+        <a href="https://www.instagram.com/"><FaInstagram /></a>
       </motion.div>
     </nav>
   );
