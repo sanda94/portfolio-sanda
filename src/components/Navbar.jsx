9d21d281d12bd8b52d1743afa8a502c0b1b0ae80
sanda94 <lsandaruwan388@gmail.com>
2024-04-30 09:08:22 +0800

    inite

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,37 @@
-import { FaFacebook, FaLinkedin } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import logo from '../assets/kevinRushLogo.png';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-200 mb-5 flex items-start justify-between py-6">
-      <motion.div 
+    <nav className="bg-red-200 mb-5 flex items-center justify-between py-6 px-4 lg:px-10">
+      <motion.div
         whileHover={{ scale: 1.2 }}
         whileTap={{ scale: 1.1 }}
         drag="x"
         dragConstraints={{ left: -100, right: 100 }}
         initial="hidden"
         animate="visible"
-        className="flex flex-shrink-0 items-start">
-        <img className="mx-6 w-40 rounded-2xl" src={logo} alt="logo"/>
+        className="flex-shrink-0"
+      >
+        <img className="w-28 rounded-2xl" src={logo} alt="logo" />
       </motion.div>
-      <motion.div 
+      <motion.div
         animate="visible"
-        className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/"><FaLinkedin /></a>
-        <a href="https://github.com/"><FaGithub /></a>
-        <a href="https://facebook.com/"><FaFacebook /></a>
-        <a href="https://www.instagram.com/"><FaInstagram /></a>
+        className="flex items-center gap-6 text-3xl lg:text-2xl"
+      >
+        <a href="https://www.linkedin.com/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/">
+          <FaGithub />
+        </a>
+        <a href="https://facebook.com/">
+          <FaFacebook />
+        </a>
+        <a href="https://www.instagram.com/">
+          <FaInstagram />
+        </a>
       </motion.div>
     </nav>
   );
